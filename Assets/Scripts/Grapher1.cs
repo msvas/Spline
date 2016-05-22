a67b930504d4bb37291f1aee6b6167d1ea151a1b
Marcelo Vasques <msouvas@gmail.com>
2016-05-22 18:32:30 -0300

    Using LineRenderer now

--- Assets/Scripts/Grapher1.cs
+++ Assets/Scripts/Grapher1.cs
@@ -2,47 +2,48 @@
 using System.Collections;
 using System.Text.RegularExpressions;
 using System;
+using System.Collections.Generic;
 
 public class Grapher1 : MonoBehaviour {
-
-    [Range(10, 100)]
-    public int resolution = 10;
+    private int pointsTotal = 4;
 
     public TextAsset curvePoints;
 
-    private ParticleSystem.Particle[] points;
-    private ParticleSystem.Particle[] splinePoints;
+    private LineRenderer line;
+    private List<Vector3> linePoints;
+    private List<Vector3> pointsFromFile;
 
     void Start() {
         curvePoints = Resources.Load("pointsdata") as TextAsset;
-        splinePoints = new ParticleSystem.Particle[resolution];
+        linePoints = new List<Vector3>();
+        pointsFromFile = new List<Vector3>();
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.SetColors(Color.yellow, Color.red);
+        line.SetWidth(0.2F, 0.2F);
     }
 
     private void CreatePoint(int id, float x, float y) {
-        splinePoints[id].position = new Vector3(x, y, 0f);
-        splinePoints[id].startColor = new Color(x, 0f, 0f);
-        splinePoints[id].startSize = 0.1f;
+        Vector3 newPoint = new Vector3(x, y, 0f);
+        linePoints.Add(newPoint);
+        line.SetPosition(id, newPoint);
     }
 
     private void ReadPointsFromFile() {
         string text = curvePoints.text;
         string[] fLines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
 
-        Debug.Log(fLines.Length);
+        pointsTotal = fLines.Length;
+        line.SetVertexCount((pointsTotal - 3)*10);
 
         for (int i = 0; i < fLines.Length; i++) {
             string valueLine = fLines[i];
             string[] pointsRead = Regex.Split(valueLine, " ");
-            points[i].position = new Vector3(float.Parse(pointsRead[0]), float.Parse(pointsRead[1]), 0f);
+            pointsFromFile.Add(new Vector3(float.Parse(pointsRead[0]), float.Parse(pointsRead[1]), 0f));
         }
     }
 
-    private Vector3 CatmullRom(int id, int t) {
-        Vector3 p0 = points[id - 1].position;
-        Vector3 p1 = points[id].position;
-        Vector3 p2 = points[id + 1].position;
-        Vector3 p3 = points[id + 2].position;
-
+    private Vector3 calculatePoints(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
         Vector3 a = 0.5f * (2f * p1);
         Vector3 b = 0.5f * (p2 - p0);
         Vector3 c = 0.5f * (2f * p0 - 5f * p1 + 4f * p2 - p3);
@@ -53,30 +54,40 @@ public class Grapher1 : MonoBehaviour {
         return pos;
     }
 
-    private void PerformSpline() {
-         ReadPointsFromFile();
+    private void CatmullRom(int id) {
+        Vector3 p0 = pointsFromFile[id - 1];
+        Vector3 p1 = pointsFromFile[id];
+        Vector3 p2 = pointsFromFile[id + 1];
+        Vector3 p3 = pointsFromFile[id + 2];
 
-        for (int i = 0; i < points.Length; i++) {
-            if ((i != 0 && i != (points.Length - 2) && i != (points.Length - 1))) {
-                Vector3 splinedPos = CatmullRom(i, 1);
-                CreatePoint(i, splinedPos.x, splinedPos.y);
+        Vector3 lastPos = new Vector3(0, 0, 0);
+
+        for (float t = 0; t < 1; t += 0.1f) {
+            Vector3 newPos = calculatePoints(t, p0, p1, p2, p3);
+
+            if (t == 0) {
+                lastPos = newPos;
+                continue;
+            }
+
+            CreatePoint((int)(id + (t * 10)), newPos.x, newPos.y);
+            lastPos = newPos;
+        }
+    }
+
+    private void PerformSpline() {
+        ReadPointsFromFile();
+ 
+        for (int i = 0; i < pointsTotal; i++) {
+            if ((i != 0 && i != (pointsTotal - 2) && i != (pointsTotal - 1))) {
+                CatmullRom(i);
             }
         }
     }
 
     void Update() {
-        if (points == null)
-        {
-            points = new ParticleSystem.Particle[resolution];
+        if (linePoints.Count < (pointsTotal - 3)*10) {
             PerformSpline();
         }
-        for (int i = 0; i < resolution; i++)
-        {
-            Vector3 p = points[i].position;
-            Color c = points[i].startColor;
-            c.g = p.y;
-            points[i].startColor = c;
-        }
-        GetComponent<ParticleSystem>().SetParticles(points, points.Length);
     }
 }
